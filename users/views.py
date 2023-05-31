160bfa76ba2c1acca565b620ddf0ecc6741c4a3a
Ahmed Adnan <ahmed666916@gmail.com>
2023-05-31 17:50:09 +0500

    fixed typo

--- users/views.py
+++ users/views.py
@@ -14,7 +14,7 @@ def sign_up(request):
             user = form.save(commit=False)
             user.username = user.username.lower()
             user.save()
-            messages.success(request, 'You have singed up successfully.')
+            messages.success(request, 'You have signed up successfully.')
             login(request, user)
             return redirect('posts')
         else:
